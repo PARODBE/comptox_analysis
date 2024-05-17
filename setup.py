f842937b5f542a05b636ee4a9581f197033bafee
Pablo Rodríguez Belenguer <parodbe@gmail.com>
2024-05-17 15:35:54 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+
+
+setup(
+    name='Highlighting_atoms_rdkit',
+    version='0.1',
+    description='Highlighting atoms function',
+    license='GNU',
+#    long_description=long_description,
+    author='Pablo Rodríguez Belenguer',
+    author_email='parodbe@gmail.com',
+    url='https://github.com/phi-grib/Highlighting_atoms_rdkit',
+    download_url='https://github.com/phi-grib/Highlighting_atoms_rdkit.git',
+    packages=find_packages(),
+    # If any package contains *.txt or *.rst files, include them:
+    # package_data={'': ['*.yaml', '*.yml']},
+    package_data={'Highlighting_atoms_rdkit': ['config.yaml']}
+)
