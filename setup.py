0f3cec273af338828ce388c59a58443a73956cef
Pablo Rodríguez Belenguer <parodbe@gmail.com>
2024-05-22 14:22:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", 'r') as f:
 
 setup(
     name='highlighting_atoms',
-    version='0.1',
+    version='0.2',
     description='Highlighting atoms function',
     license='GNU',
     long_description=long_description,
