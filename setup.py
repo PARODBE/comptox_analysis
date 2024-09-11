4a70fbf9297a208bfd012855faf090a39d747b90
Pablo Rodríguez Belenguer <parodbe@gmail.com>
2024-09-11 10:11:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", 'r') as f:
 
 setup(
     name='comptox_analysis',
-    version='0.8',
+    version='0.9',
     description='ML analysis',
     license='GNU',
     long_description=long_description,
